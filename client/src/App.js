8725487175530518ec56287a842c73167fed37b1
SONAL04KHATRI <161185466+SONAL04KHATRI@users.noreply.github.com>
2025-02-09 22:57:29 +0530

    "Added client/src/App.js with Chat component integration"

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Real-Time Chat App</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
